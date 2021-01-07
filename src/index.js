8233899181250495143622c57056965017712375
Derek Mayer <deverex1317@gmail.com>
2021-01-07 16:57:25 -0600

    Some more minor cleanup

--- src/index.js
+++ src/index.js
@@ -1,113 +1,107 @@
 'use strict';
 
-function SudokuSolver() {
-  /*
-   * Check if the number is a legal candidate
-   * for the given cell
-   */
-  function checkCandidate(puzzleArray, currentNumber, row, column) {
-    let validCandidate = true;
+// Check if the number is a legal candidate for the given cell
+function checkCandidate(puzzleArray, currentNumber, row, column) {
+  let validCandidate = true;
 
-    for (let i = 0; i < 9; i++) {
-      let bIndex =
-        ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) *
-        9 +
-        (Math.floor(column / 3) * 3) +
-        (i % 3);
+  for (let i = 0; i < 9; i++) {
+    let bIndex =
+      ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) *
+      9 +
+      (Math.floor(column / 3) * 3) +
+      (i % 3);
 
-      if (currentNumber == puzzleArray[(row * 9) + i] ||
-        currentNumber == puzzleArray[column + (i * 9)] ||
-        currentNumber == puzzleArray[bIndex]
-      ) {
-        validCandidate = false;
+    if (currentNumber == puzzleArray[(row * 9) + i] ||
+      currentNumber == puzzleArray[column + (i * 9)] ||
+      currentNumber == puzzleArray[bIndex]
+    ) {
+      validCandidate = false;
 
-        break;
-      }
+      break;
     }
-
-    return validCandidate;
   }
 
-  /*
-   * Test all possible numbers for a given cell until
-   * the puzzle is solved
-   */
-  function getCandidate(index, puzzleArray) {
-    if (index >= puzzleArray.length) {
-      return true;
-    } else if (puzzleArray[index] != 0) {
-      return getCandidate(index + 1, puzzleArray);
-    }
+  return validCandidate;
+}
 
-    for (let i = 1; i <= 9; i++) {
-      const currentNumber = i;
-      const row = Math.floor(index / 9);
-      const column = index % 9;
-
-      if (checkCandidate(puzzleArray, i, row, column)) {
-        puzzleArray[index] = i;
-
-        if (getCandidate(index + 1, puzzleArray)) {
-          return true;
-        }
-      }
-    }
-
-    puzzleArray[index] = 0;
-
-    return false;
+// Test all possible numbers for a given cell until the puzzle is solved
+function getCandidate(index, puzzleArray) {
+  if (index >= puzzleArray.length) {
+    return true;
+  } else if (puzzleArray[index] != 0) {
+    return getCandidate(index + 1, puzzleArray);
   }
 
-  function chunkIntoGroups(puzzleArray) {
-    let result = [];
+  for (let i = 1; i <= 9; i++) {
+    const currentNumber = i;
+    const row = Math.floor(index / 9);
+    const column = index % 9;
 
-    const groupAmount = 9;
+    if (checkCandidate(puzzleArray, i, row, column)) {
+      puzzleArray[index] = i;
 
-    for (let i = 0; i < puzzleArray.length; i += groupAmount) {
-      result.push(puzzleArray.slice(i, i + groupAmount));
-    }
-
-    return result;
-  }
-
-  function validatePuzzleInput(puzzle) {
-    if (puzzle.length !== 81) {
-      throw new Error('Puzzle has invalid length.');
-    }
-
-    for (let i = 0; i < puzzle.length; i++) {
-      if (isNaN(puzzle[i])) {
-        throw new Error('Invalid puzzle', `In position ${i}: ${puzzle[i]}`);
+      if (getCandidate(index + 1, puzzleArray)) {
+        return true;
       }
     }
   }
 
-  function formatPuzzle(solution) {
-    const chunked = chunkIntoGroups(solution);
-    let formattedPuzzle = '';
+  puzzleArray[index] = 0;
 
-    for (let i = 0; i < chunked.length; i++) {
-      formattedPuzzle += `${chunked[i].join(' | ')}\n`;
-      formattedPuzzle += `${'-'.repeat(chunked[i].length * 4)}\n`;
-    }
+  return false;
+}
 
-    return formattedPuzzle;
+function chunkIntoGroups(puzzleArray) {
+  let result = [];
+
+  const groupAmount = 9;
+
+  for (let i = 0; i < puzzleArray.length; i += groupAmount) {
+    result.push(puzzleArray.slice(i, i + groupAmount));
   }
 
-  this.solve = function (puzzle, formatResults = true) {
-    validatePuzzleInput(puzzle);
+  return result;
+}
 
-    const puzzleArray = puzzle.split('').map((v) => parseInt(v));
+function validatePuzzleInput(puzzle) {
+  if (puzzle.length !== 81) {
+    throw new Error('Puzzle has invalid length.');
+  }
 
-    // Start solving on index 0
-    const solution = getCandidate(0, puzzleArray);
-
-    if (!solution) {
-      throw new Error('No solution found.')
+  for (let i = 0; i < puzzle.length; i++) {
+    if (isNaN(puzzle[i])) {
+      throw new Error('Invalid puzzle', `In position ${i}: ${puzzle[i]}`);
     }
-
-    return formatResults ? formatPuzzle(puzzleArray) : chunkIntoGroups(puzzleArray);
   }
 }
 
-module.exports = SudokuSolver;
+function formatPuzzle(solution) {
+  const chunked = chunkIntoGroups(solution);
+  let formattedPuzzle = '';
+
+  for (let i = 0; i < chunked.length; i++) {
+    formattedPuzzle += `${chunked[i].join(' | ')}\n`;
+    formattedPuzzle += `${'-'.repeat(chunked[i].length * 4)}\n`;
+  }
+
+  return formattedPuzzle;
+}
+
+function solve(puzzle, formatResults = true) {
+  validatePuzzleInput(puzzle);
+
+  const puzzleArray = puzzle.split('').map((v) => parseInt(v));
+
+  // Start solving on index 0
+  const solution = getCandidate(0, puzzleArray);
+
+  if (!solution) {
+    throw new Error('No solution found.')
+  }
+
+  return formatResults ? formatPuzzle(puzzleArray) : chunkIntoGroups(puzzleArray);
+}
+
+module.exports = {
+  solve
+};
--- test/unit/src/index.spec.js
+++ test/unit/src/index.spec.js
@@ -1,43 +1,11 @@
 'use strict';
 
 let test = require('tape');
-let SudokuSolver = require('../../../src/index');
-
-test('Is a valid puzzle with a solution, and returns a formatted puzzle', (t) => {
-  t.plan(1);
-
-  let solver = new SudokuSolver();
-
-  let result = solver.solve('001700509573024106800501002700295018009400305652800007465080071000159004908007053');
-
-  t.same(result,
-    '2 | 4 | 1 | 7 | 6 | 8 | 5 | 3 | 9\n------------------------------------\n5 | 7 | 3 | 9 | 2 | 4 | 1 | 8 | 6\n------------------------------------\n8 | 9 | 6 | 5 | 3 | 1 | 7 | 4 | 2\n------------------------------------\n7 | 3 | 4 | 2 | 9 | 5 | 6 | 1 | 8\n------------------------------------\n1 | 8 | 9 | 4 | 7 | 6 | 3 | 2 | 5\n------------------------------------\n6 | 5 | 2 | 8 | 1 | 3 | 4 | 9 | 7\n------------------------------------\n4 | 6 | 5 | 3 | 8 | 2 | 9 | 7 | 1\n------------------------------------\n3 | 2 | 7 | 1 | 5 | 9 | 8 | 6 | 4\n------------------------------------\n9 | 1 | 8 | 6 | 4 | 7 | 2 | 5 | 3\n------------------------------------\n');
-});
-
-test('Is a valid puzzle with a solution', (t) => {
-  t.plan(1);
-
-  let solver = new SudokuSolver();
-
-  let result = solver.solve('001700509573024106800501002700295018009400305652800007465080071000159004908007053', false);
-
-  t.same(result, [
-    [2, 4, 1, 7, 6, 8, 5, 3, 9],
-    [5, 7, 3, 9, 2, 4, 1, 8, 6],
-    [8, 9, 6, 5, 3, 1, 7, 4, 2],
-    [7, 3, 4, 2, 9, 5, 6, 1, 8],
-    [1, 8, 9, 4, 7, 6, 3, 2, 5],
-    [6, 5, 2, 8, 1, 3, 4, 9, 7],
-    [4, 6, 5, 3, 8, 2, 9, 7, 1],
-    [3, 2, 7, 1, 5, 9, 8, 6, 4],
-    [9, 1, 8, 6, 4, 7, 2, 5, 3]
-  ]);
-});
 
 test('Is an invalid puzzle', (t) => {
   t.plan(1);
 
-  let solver = new SudokuSolver();
+  let solver = require('../../../src/index');
 
   let actualError;
   const expectedErrorMessage = 'Puzzle has invalid length.';
@@ -54,7 +22,7 @@ test('Is an invalid puzzle', (t) => {
 test('Is a valid puzzle without a solution', (t) => {
   t.plan(1);
 
-  let solver = new SudokuSolver();
+  let solver = require('../../../src/index');
 
   let actualError;
   const expectedErrorMessage = 'No solution found.';
@@ -66,4 +34,35 @@ test('Is a valid puzzle without a solution', (t) => {
   }
 
   t.equal(actualError.message, expectedErrorMessage);
+});
+
+test('Is a valid puzzle with a solution, and returns a formatted puzzle', (t) => {
+  t.plan(1);
+
+  let solver = require('../../../src/index');
+
+  let result = solver.solve('001700509573024106800501002700295018009400305652800007465080071000159004908007053');
+
+  t.same(result,
+    '2 | 4 | 1 | 7 | 6 | 8 | 5 | 3 | 9\n------------------------------------\n5 | 7 | 3 | 9 | 2 | 4 | 1 | 8 | 6\n------------------------------------\n8 | 9 | 6 | 5 | 3 | 1 | 7 | 4 | 2\n------------------------------------\n7 | 3 | 4 | 2 | 9 | 5 | 6 | 1 | 8\n------------------------------------\n1 | 8 | 9 | 4 | 7 | 6 | 3 | 2 | 5\n------------------------------------\n6 | 5 | 2 | 8 | 1 | 3 | 4 | 9 | 7\n------------------------------------\n4 | 6 | 5 | 3 | 8 | 2 | 9 | 7 | 1\n------------------------------------\n3 | 2 | 7 | 1 | 5 | 9 | 8 | 6 | 4\n------------------------------------\n9 | 1 | 8 | 6 | 4 | 7 | 2 | 5 | 3\n------------------------------------\n');
+});
+
+test('Is a valid puzzle with a solution', (t) => {
+  t.plan(1);
+
+  let solver = require('../../../src/index');
+
+  let result = solver.solve('001700509573024106800501002700295018009400305652800007465080071000159004908007053', false);
+
+  t.same(result, [
+    [2, 4, 1, 7, 6, 8, 5, 3, 9],
+    [5, 7, 3, 9, 2, 4, 1, 8, 6],
+    [8, 9, 6, 5, 3, 1, 7, 4, 2],
+    [7, 3, 4, 2, 9, 5, 6, 1, 8],
+    [1, 8, 9, 4, 7, 6, 3, 2, 5],
+    [6, 5, 2, 8, 1, 3, 4, 9, 7],
+    [4, 6, 5, 3, 8, 2, 9, 7, 1],
+    [3, 2, 7, 1, 5, 9, 8, 6, 4],
+    [9, 1, 8, 6, 4, 7, 2, 5, 3]
+  ]);
 });
